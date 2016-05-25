b17b304219194f317dc0d46d14841bef5535a4cf
kakuilan <kakuilan@163.com>
2016-05-25 17:14:48 +0800

    Update backupOnlyPhp.sh

--- backupOnlyPhp.sh
+++ backupOnlyPhp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #只压缩备份目录下PHP文件
 fname=$(date +%Y%m%d%H%M%S).backupphp.tar.gz
+#find /home/testcode/ -type f -regex ".*\.\(php\|js\|html\|css\)" | xargs tar czvf $fname
 find /home/testcode/ -type f -regex ".*\.\(php\)" | xargs tar czvf $fname
 echo $fname "backup only php done."
