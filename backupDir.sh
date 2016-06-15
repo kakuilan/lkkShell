c279d496df3b6c6abb04b7cd3562295f921f2aab
lianjiagui <lianjiagui@vengood.com>
2016-06-15 16:57:35 +0800

    add 备份代码目录

---
+++ backupDir.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#备份代码目录
+
+#要备份的代码目录
+BackupDir="/home/testcode/project/"
+
+#保存备份文件的目录
+SaveDir="/home/testcode/"
+
+#备份文件名
+BackupFile=${SaveDir}$(date +%Y%m%d%H%M%S).codebackup.tar.gz
+
+#进入目录
+if cd $BackupDir ;then
+	echo "cd dir:"
+	pwd
+else
+	echo "cannot cd $BackupDir"
+	exit 0
+fi
+
+find . -type f -regex ".*\.\(php\|html\|js\|css\)" -print0 | xargs --null tar --ignore-failed-read -czvf ${BackupFile}
+
+if [ ! -f "$BackupFile" ] ;then
+	echo "backup directory happen error!"
+	exit 0
+else
+	echo "backup directory done:"$BackupFile
+	exit 1
+fi
--- packGitChanges.sh
+++ packGitChanges.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #打包GIT两个版本之间变更的文件列表
-fname=$(date +%Y%m%d%H%M%S).gitUpdate.tar.gz
-dirname=/root/mycode
+
+#git项目目录
+$ProjectDir=/root/mycode/project
+
+#保存备份文件的目录
+SaveDir="/home/testcode/"
+
+#备份文件名
+PackFile=${SaveDir}$(date +%Y%m%d%H%M%S).gitUpdate.tar.gz
 
 #获取输入的git两个版本号
 echo -n "Enter firstVersionCode and press [ENTER]: "
@@ -21,20 +28,20 @@ echo "firstcode: "$firstcode
 echo "secondcode: "$secondcode
 
 #进入目录
-if cd $dirname ;then
+if cd $ProjectDir ;then
 	echo "cd dir:"
 	pwd
 else
-	echo "cannot cd $dirname"
+	echo "cannot cd $ProjectDir"
 	exit 0
 fi
 
 #git对比并打包
-git diff $firstcode $secondcode --name-only | xargs tar --ignore-failed-read -czvf $fname
-if [ ! -f "$fname" ] ;then
+git diff $firstcode $secondcode --name-only | xargs --null tar --ignore-failed-read -czvf $PackFile
+if [ ! -f "$PackFile" ] ;then
 	echo "pack git update files happen error."
 	exit 0
 else
-	echo "pack git update files done:"$fname
+	echo "pack git update files done:"$PackFile
 	exit 1
 fi
