9344860b90b11281868e74ed0f4721a473f4cd91
kakuilan <kakuilan@163.com>
2017-07-17 09:58:26 +0800

    git branch

--- packGitChanges.sh
+++ packGitChanges.sh
@@ -10,6 +10,13 @@ SaveDir="/home/testcode/"
 #备份文件名
 PackFile=${SaveDir}$(date +%Y%m%d%H%M%S).gitUpdate.tar.gz
 
+#获取git分支
+echo -n "Enter git-branch and press [ENTER],default(master): "
+read -r branch
+if [ ! -n "$branch" ];then
+  branch="master"
+fi
+
 #获取输入的git两个版本号
 echo -n "Enter firstVersionCode and press [ENTER]: "
 read -r firstcode
@@ -36,10 +43,20 @@ else
 	exit 0
 fi
 
-#git对比并打包
+//切换所选分支
 git pull
+if [ "$branch" != "master" ]; then  
+	git checkout $branch
+fi
+
+#git对比并打包
 #git diff $firstcode $secondcode --name-only | xargs tar --ignore-failed-read -czvf $PackFile
 git diff $firstcode $secondcode --name-only |grep '^[^.].*[^.sql]$'| xargs tar --ignore-failed-read -czvf $PackFile
+
+#切回主分支
+if [ "$branch" != "master" ]; then  
+	git checkout master
+fi
 
 if [ ! -f "$PackFile" ] ;then
 	echo "pack git update files happen error."
